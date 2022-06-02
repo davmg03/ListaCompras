355a8e992a46713a766905b94b2ef7c07d2ca059
dmg <davidmunozg.03@gmail.com>
2022-06-02 15:15:18 -0500

    modificiacion al index y js

--- js/main.js
+++ js/main.js
@@ -42,8 +42,12 @@ agregar.addEventListener("click", (event) =>{
 
  // console.log(txtNombre.value, txtNumber.value);
 
+let contador = 0;
+let costoTotal = 0;
 let element = document.getElementById("totalPrecio");
 element.innerHTML="Total en precio";
+
+let total= document.getElementById("precioTotal");
 
 let txtNombre = document.getElementById("Name");
 
@@ -51,15 +55,61 @@ let txtNumber = document.getElementById("Number");
 
 let tabla = document.getElementById("tablaListaCompras");
 let cuerpoTabla = tabla.getElementsByTagName("tbody");
+function validarNombre(){
+    if (txtNombre.value.length <3) {
+        return false;
+    }//if
+    return true;
+}// validarNombre
+function validarCantidad(){
+    if(txtNumber.value.length==0) {
+        return false;
+    }// if
+     if (isNaN(txtNumber.value)){
+        return false;
+     }//if
 
+     if (parseFloat(txtNumber.value)<=0) {
+        return false;
+     }//if
+     return true;
+}// validarCantidad
 let agregar = document.getElementById("btnAgregar");
 agregar.addEventListener("click", (event) =>{
-    let precio = Math.random() * 50;
+    event.preventDefault();
+    if ((! validarNombre()) || (! validarCantidad())){
+        let lista="";
+        if (!validarNombre()){
+            txtNombre.style.border="red thin solid";
+            lista+="<li> Se debe escribir un nombre valido</li>";
+        }//
+        if (!validarCantidad()){
+            txtNumber.style.border="red thin solid";
+            lista+="<li> Se debe escribir una cantidad valida</li>";
+        } 
+        document.getElementById("alertValidacionesTexto").innerHTML=`Los campos deben ser llamados correctamente <ul>${lista} </ul>`;
+        document.getElementById("alertValidaciones").style.display="block";
+        setTimeout( function(){
+        document.getElementById("alertValidaciones").style.display="none";
+        },
+        5000
+        );
+        return false;
+    }//if
+    txtNumber.style.border="";
+    txtNombre.style.border="";
+    document.getElementById("alertValidaciones").style.display="none";
+    contador++;
+    document.getElementById("contadorProductos").innerHTML=contador;
+    let precio = Math.floor((Math.random() * 50)*100)/100;
+    let cantidad= parseFloat(txtNumber.value);
+    costoTotal += (precio*cantidad);
+    total.innerHTML = `$ ${costoTotal}`;
     let tmp = `<tr>
-    <th scope="row">1</th>
+    <th scope="row">${contador}</th>
     <td>${txtNombre.value}</td>
     <td>${txtNumber.value}</td>
-    <td>$ ${precio}</td>
+    <td>$ ${precio.toFixed(2)}</td>
     </tr>`;
     console.log(tmp);
     cuerpoTabla[0].innerHTML += tmp;
@@ -68,3 +118,14 @@ agregar.addEventListener("click", (event) =>{
     txtNombre.focus();
 }
 );
+txtNombre.addEventListener("blur", (event)=>{
+    event.target.value = event.target.value.trim();
+}
+);
+txtNumber.addEventListener("blur", (event)=>{
+    event.target.value = event.target.value.trim();
+}
+);
+
+
+
